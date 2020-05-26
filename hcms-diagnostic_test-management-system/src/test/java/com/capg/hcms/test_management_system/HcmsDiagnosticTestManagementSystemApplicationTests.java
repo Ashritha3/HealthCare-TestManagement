7098dbb86a6be59391f6c134a8662ff0abbbdbaa
Ashritha <Ashritha@DESKTOP-72O2310>
2020-05-26 13:27:10 +0530

    Updated

--- hcms-diagnostic_test-management-system/src/test/java/com/capg/hcms/test_management_system/HcmsDiagnosticTestManagementSystemApplicationTests.java
+++ hcms-diagnostic_test-management-system/src/test/java/com/capg/hcms/test_management_system/HcmsDiagnosticTestManagementSystemApplicationTests.java
@@ -14,43 +14,43 @@ import com.capg.hcms.test_management_system.model.DiagnosticTest;
 import com.capg.hcms.test_management_system.repository.IDiagnosticTestRepo;
 import com.capg.hcms.test_management_system.service.DiagnosticTestServiceImpl;
 
-//@SpringBootTest
+@SpringBootTest
 class HcmsDiagnosticTestManagementSystemApplicationTests {
 	
-//	@Autowired
-//	IDiagnosticTestRepo repo;
-//	
-//	@Autowired
-//	DiagnosticTestServiceImpl service;
-//	DiagnosticTest test1;
-//	DiagnosticTest test2;
-//	
-//	@BeforeEach
-//	public void init() {
-//		test1 = new DiagnosticTest("", "LFT");
-//		test2 = new DiagnosticTest("59659", "Blood Sugar");
-//	}
-//	
-//	@Test
-//	public void testAddtest() {
-//		service.addTest(test1);
-//		assertEquals(true,repo.getByTestName(test1.getTestName())!=null);
-//	}
-//	
-//	@Test
-//	public void testRemovetest() {
-//		service.removeTest(test2);
-//		assertEquals(false,repo.getByTestName(test2.getTestName())!=null);
-//	}
-//	
-//	@Test
-//	public void testException() {
-//		Throwable exception = assertThrows(TestNotFoundException.class, ()->{
-//			service.removeTest(test2);
-//		});
-//		assertEquals("["+test2.getTestId()+"] is Not Found", exception.getMessage());
-//	}
-//	
+	@Autowired
+	IDiagnosticTestRepo repo;
+	
+	@Autowired
+	DiagnosticTestServiceImpl service;
+	DiagnosticTest test1;
+	DiagnosticTest test2;
+	
+	@BeforeEach
+	public void init() {
+		test1 = new DiagnosticTest("", "LFT");
+		test2 = new DiagnosticTest("84409", "MRI scanning");
+	}
+	
+	@Test
+	public void testAddtest() {
+		service.addTest(test1);
+		assertEquals(true,repo.getByTestName(test1.getTestName())!=null);
+	}
+	
+	@Test
+	public void testRemovetest() {
+		service.removeTest(test2);
+		assertEquals(false,repo.getByTestName(test2.getTestName())!=null);
+	}
+	
+	@Test
+	public void testException() {
+		Throwable exception = assertThrows(TestNotFoundException.class, ()->{
+			service.removeTest(test2);
+		});
+		assertEquals("["+test2.getTestId()+"] is Not Found", exception.getMessage());
+	}
+	
 //	@Test
 //	public void testException1() {
 //		Throwable exception = assertThrows(ListEmptyException.class, ()->{
